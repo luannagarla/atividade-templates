9513bcf4a9de3aba5934d9301f97dee698bf5c5e
Luanna Garla <luagarla@gmail.com>
2025-02-08 12:42:48 -0300

    Tentativa matriculas

--- Atividade 01/Matriculas.hpp
+++ Atividade 01/Matriculas.hpp
@@ -7,5 +7,10 @@ class Matriculas : public Disciplina {
 private:
 	int numAlunos;
 public:
-	
+	Matriculas (long CD, string No, int nAlunos) : Disciplina(CD, No), numAlunos(nAlunos) { }
+
+	friend ostream &operator << (ostream &S, const Matriculas &M) {
+		S << "Disciplina: " << M.Codigo_ << '\t' << M.Nome_ << "Quantidade de alunos: " << M.numAlunos<< endl;
+		return S;
+	}
 };
